8dd09e606b2b86b4f5c7d8c8680b022e793e6bdb
gmendesfonseca <fonseca.gabriel.mendes@gmail.com>
2024-09-30 09:52:45 -0300

    feat :sparkles: change .env

--- src/header-app/config/links.ts
+++ src/header-app/config/links.ts
@@ -1,15 +1,13 @@
 export type Modulelinks = {
   api: string;
   web: string;
-  production: boolean;
   secret_key: string;
   env: string;
 };
 
 export const links: Modulelinks = {
-  api: import.meta.env.VITE_API_URL,
-  web: import.meta.env.VITE_APP_PRODUCTION,
-  production: import.meta.env.VITE_APP_PRODUCTION!,
-  secret_key: import.meta.env.JWT_SECRET!,
+  api: import.meta.env.VITE_APP_API_URL,
+  web: import.meta.env.VITE_APP_WEB_URL,
+  secret_key: import.meta.env.VITE_APP_JWT_SECRET,
   env: import.meta.env.MODE,
 };
