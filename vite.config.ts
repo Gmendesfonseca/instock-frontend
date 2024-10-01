d5e1507e142b9e08fa654d9167ed0697999f90df
GABRIEL MENDES FONSECA <124696627+Gmendesfonseca@users.noreply.github.com>
2024-09-30 21:53:15 -0300

    build :rocket: add vite config to absolute import

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 const env = loadEnv('', process.cwd());
+function pathResolve(dir: string) {
+  return path.resolve(__dirname, '.', dir);
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: { '@': pathResolve('src') },
+  },
   server: {
     port: parseInt(env.PORT || '3000'),
   },
