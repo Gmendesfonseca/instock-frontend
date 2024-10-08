17361a00a9c94bdf7dc7171959abf962c49e3fea
gmendesfonseca <fonseca.gabriel.mendes@gmail.com>
2024-10-08 09:08:49 -0300

    feat :sparkles: change on vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,21 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
-const env = loadEnv('', process.cwd());
 function pathResolve(dir: string) {
   return path.resolve(__dirname, '.', dir);
 }
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: { '@': pathResolve('src') },
-  },
-  plugins: [react()],
-  server: {
-    port: parseInt(env.PORT),
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    resolve: {
+      alias: { '@': pathResolve('src') },
+    },
+    plugins: [react()],
+    server: {
+      port: parseInt(env.PORT),
+    },
+  };
 });
